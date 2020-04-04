c572f519c2453875f2552be041a1a6036e2d71f1
fehmi009 <50001325+fehmi009@users.noreply.github.com>
2020-04-04 19:05:02 -0400

    2nd push

--- src/test/java/chrome/SearchKeyWords.java
+++ src/test/java/chrome/SearchKeyWords.java
@@ -1,15 +1,12 @@
 package chrome;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SearchKeyWords {
 
